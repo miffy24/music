97f0cfa596e260defadd614c1dc469978d1c2d4c
chenzhutao <1248378683@qq.com>
2018-04-08 13:09:47 +0800

    点击歌曲时激活列表项

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -10,15 +10,22 @@
             let $el = $(this.el)
             $el.html(this.template)
             let {songs}=data
-            let liList = songs.map((song)=> $('<li></li>').text(song.name))
+            let liList = songs.map((song)=> 
+                $('<li></li>').text(song.name).attr('data-song-id',song.id))
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
         },
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active').removeClass('active')
+        },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
-        }
+        },
+
     }
     let model={
         data:{
@@ -30,7 +37,6 @@
                 this.data.songs = songs.map((song)=>{
                     return {id:song.id,...song.attributes}
                 })
-            
             });            
         }
     }
@@ -38,15 +44,30 @@
         init(view,model){
             this.view = view
             this.model = model
+            this.findAllSongs()
+            this.bindEvents()
             this.view.render(this.model.data)
+            this.bindEventHubs()
+
+        },
+        findAllSongs(){
+            return this.model.find().then(()=>{
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select',{id:songId})
+            })
+        },
+        bindEventHubs(){
             window.eventHub.on('upload',(data)=>{
                 this.view.clearActive()
             })
             window.eventHub.on('create',(songData)=>{
                 this.model.data.songs.push(songData)
-                this.view.render(this.model.data)
-            })
-            this.model.find().then(()=>{
                 this.view.render(this.model.data)
             })
         }
